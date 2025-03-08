7b49fc5911feeccfd5e0850da4a22ef6234ffc92
Dean Nevins <dean@castlebartech.com>
2025-03-08 00:19:26 -0800

    First released version.

--- src/genpull/gpull.py
+++ src/genpull/gpull.py
@@ -6,6 +6,7 @@ from xlrd import XLRDError
 from typing_extensions import Annotated
 from jinja2 import Environment, PackageLoader, select_autoescape
 from datetime import date
+from .tex_escape import tex_escape
 
 env = Environment(
     loader=PackageLoader("genpull"),
@@ -43,7 +44,7 @@ def strip_dates(date_str: str) -> str:
 def gpull(
   debug: Annotated[bool, typer.Option(help="Show debugging information. (lots of output)")] = False,
   infilename: Annotated[str, typer.Argument(help="The League of Comic Geeks Export Pulls xls filename.")] = "Pulls-ComicGeeks.xls",
-  outfilename: Annotated[str, typer.Option(help="Name of LaTeX output file.")] = None,
+  output: Annotated[str, typer.Option(help="Name of LaTeX output file.")] = None,
   username: Annotated[str, typer.Option(help="Name of subscription list owner.")] = "Nevins"
 ):
   """Creates a LaTeX file for a subscription list from the League of
@@ -53,10 +54,6 @@ def gpull(
     subscriptions along with that week's pulls. My LCS wants a PDF
     which has all of the current subscritions listed. This code creates
     a LaTeX file that gives a nice printed version once processed.
-
-    Args:
-      infilename:
-        Name of the League of Comic Geeks export file.
 
     Raises:
       FileNotFoundError:
@@ -86,27 +83,37 @@ def gpull(
 
           logging.debug(f"{strip_dates(row[0].value), row[1].value}")
           if row[1].value not in subscriptions:
-            subscriptions[row[1].value] = []
-          subscriptions[row[1].value].append(strip_dates(row[0].value))
+            subscriptions[tex_escape(row[1].value)] = []
+          subscriptions[row[1].value].append(tex_escape(strip_dates(row[0].value)))
           num_comics += 1
 
-  
+      # Sort the Company names
+      companies = list(subscriptions)
+      logging.debug(f"\nRaw Company Names\n{companies}")
+      companies.sort(key=str.casefold)
+      logging.debug(f"\nSorted Company Names\n{companies}")
+      
+      for s in companies:
+        subscriptions[s].sort(key=str.casefold)  
+            
+      logging.debug("RENDERING")
+      template = env.get_template("subscriptions.jinja")
+
+      if not output:
+        print(template.render(num_comics=num_comics, 
+                      date=date.today().strftime("%Y %m %d"),
+                      username=username, 
+                      sorted_subscriptions=companies, 
+                      subscriptions=subscriptions))
+      else:
+        with open(output, 'w', encoding="utf-8") as subscription_tex:
+          subscription_tex.write(template.render(num_comics=num_comics, 
+                      date=date.today().strftime("%Y %m %d"),
+                      username=username, 
+                      sorted_subscriptions=companies, 
+                      subscriptions=subscriptions))
+
   except FileNotFoundError as e:
       print(f"genpull error: {e}")
   except XLRDError as e:
       print(f"genpull error: {e} in the xls file.")
-
-  subscription_keys = list(subscriptions)
-  logging.debug(f"\nRaw Subscription Keys\n{subscription_keys}")
-  subscription_keys.sort(key=str.casefold)
-  for s in subscription_keys:
-    subscriptions[s].sort(key=str.casefold)  
-  logging.debug(f"\nSorted Subscription Keys\n{subscription_keys}")
-  logging.debug("RENDERING")
-  template = env.get_template("subscriptions.jinja")
-
-  print(template.render(num_comics=num_comics, 
-                        date=date.today().strftime("%Y %m %d"),
-                        username=username, 
-                        sorted_subscriptions=subscription_keys, 
-                        subscriptions=subscriptions))
